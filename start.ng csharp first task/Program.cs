9e117b45a0fd30e22a314d33137d162f5eeee2d4
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-19 22:03:25 +0100

    Add project files.

---
+++ start.ng csharp first task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace start.ng_csharp_first_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber;
+            int secondnumber;
+            int thirdnumber;
+            int result;
+
+            Console.Write("Enter your first number: ");
+            firstnumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your second number: ");
+            secondnumber = Int32.Parse(Console.ReadLine());
+            Console.Write("Please Enter your third number: ");
+            thirdnumber = Int32.Parse(Console.ReadLine());
+
+            result = firstnumber * secondnumber * thirdnumber;
+            Console.WriteLine("Your result is " + result);
+
+
+
+        }
+    }
+}
